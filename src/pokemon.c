ba67a6168d84a423c859fcee2bb7719a4cf809e9
Daydeus <jl02murphy@gmail.com>
2023-11-28 21:11:38 -0600

    Implement ability Technician.

--- src/pokemon.c
+++ src/pokemon.c
@@ -2502,6 +2502,8 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         gBattleMovePower = (150 * gBattleMovePower) / 100;
     if (type == TYPE_BUG && attacker->ability == ABILITY_SWARM && attacker->hp <= (attacker->maxHP / 3))
         gBattleMovePower = (150 * gBattleMovePower) / 100;
+    if (attacker->ability == ABILITY_TECHNICIAN && gBattleMoves[move].power <= 60)
+        gBattleMovePower = (150 * gBattleMovePower) / 100;
 
     // Self-destruct / Explosion cut defense in half
     if (gBattleMoves[gCurrentMove].effect == EFFECT_EXPLOSION)
